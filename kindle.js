ab0a48eabab9424ed72b246032de9e148d6576ff
Miguel Lederkremer <mlederkremer@gmail.com>
2019-08-19 17:38:09 -0300

    agregado el método add

--- kindle.js
+++ kindle.js
@@ -8,9 +8,13 @@ class Kindle {
 		this._library = [];
 	}
 	add(eBook) {
-		this._library.push(eBook);
-		this.notYetReadBooks++;
-	}    
+		if (this._library.some(x => x.title == eBook.title)) {
+			console.warn(`"${eBook.title}" already exists in library`);
+		} else {
+			this._library.push(eBook);
+			this.notYetReadBooks++;
+		}
+	}
 }
 
 class Ebook {
@@ -26,9 +30,59 @@ class Ebook {
 
 let kindle = new Kindle();
 
-kindle.add(new Ebook ('Drive', 'Psychology', 'Daniel H. Pink', 'https://i.imgur.com/B3XpOxl.jpg'));
-kindle.add(new Ebook ('Futuro Pop','Science Fiction','Luciano Banchero','https://i.imgur.com/uYOy55M.jpg'));
-kindle.add(new Ebook ('The Lord of the Rings - The Fellowship of the Ring', 'Fantasy','J. R. R. Tolkien','https://i.imgur.com/OwMUnQu.jpg'));
-kindle.add(new Ebook ('The Principles Of Object-oriented Javascript', 'Programming','Nicholas C. Zakas', 'https://i.imgur.com/Iktw1ps.jpg'));
-kindle.add(new Ebook ("Harry Potter and the Philosopher's Stone", 'Fantasy','J. K. Rowling', 'https://i.imgur.com/PH1aXaP.jpg'));
-kindle.add(new Ebook ('Eloquent JavaScript', 'Programming', 'Marijn Haverbeke',  'https://i.imgur.com/F4NQlvx.jpg'));
+kindle.add(
+	new Ebook(
+		"Drive",
+		"Psychology",
+		"Daniel H. Pink",
+		"https://i.imgur.com/B3XpOxl.jpg"
+	)
+);
+kindle.add(
+	new Ebook(
+		"Futuro Pop",
+		"Science Fiction",
+		"Luciano Banchero",
+		"https://i.imgur.com/uYOy55M.jpg"
+	)
+);
+kindle.add(
+	new Ebook(
+		"The Lord of the Rings - The Fellowship of the Ring",
+		"Fantasy",
+		"J. R. R. Tolkien",
+		"https://i.imgur.com/OwMUnQu.jpg"
+	)
+);
+kindle.add(
+	new Ebook(
+		"The Principles Of Object-oriented Javascript",
+		"Programming",
+		"Nicholas C. Zakas",
+		"https://i.imgur.com/Iktw1ps.jpg"
+	)
+);
+kindle.add(
+	new Ebook(
+		"Harry Potter and the Philosopher's Stone",
+		"Fantasy",
+		"J. K. Rowling",
+		"https://i.imgur.com/PH1aXaP.jpg"
+	)
+);
+kindle.add(
+	new Ebook(
+		"Eloquent JavaScript",
+		"Programming",
+		"Marijn Haverbeke",
+		"https://i.imgur.com/F4NQlvx.jpg"
+	)
+);
+kindle.add(
+	new Ebook(
+		"Eloquent JavaScript",
+		"Programming",
+		"Marijn Haverbeke",
+		"https://i.imgur.com/F4NQlvx.jpg"
+	)
+);
